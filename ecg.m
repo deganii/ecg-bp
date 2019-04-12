96e96b1c6c0bdafde2900a0afd7504df326decc0
deganii <idegani@mit.edu>
2019-04-12 17:14:28 -0400

    add units (mV / s)

--- ecg.m
+++ ecg.m
@@ -10,8 +10,18 @@ A = fread(fileID, [5000],'float');
 fclose(fileID);
 
 fs = 125; % ECG Sample Rate in Hz
+A = A - mean(A); % center around 0
 time_sec = (1:length(A))/fs;
+
+% Scale to mV:
+% Sample peak: 1.35mV = [10.75 - (-5.35)] e-35 * scale_factor
+scale_factor = 0.0838509e35;
+
 % plot(time_sec, log10(A))
 % plot(time_sec, log(A))
-plot(time_sec, A)
+plot(time_sec, A*scale_factor)
+
 xlabel('Time (s)')
+ylabel('Voltage (mV)')
+
+
